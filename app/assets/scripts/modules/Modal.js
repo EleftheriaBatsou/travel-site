4973e2f36b3f5bd508cb2c0d32070a69d270f43e
Eleftheria <eleftheriabatsou@gmail.com>
2016-12-07 22:45:33 +0100

    fixing bugs with images

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -8,22 +8,16 @@ class Modal {
     this.events();
   }
 
-
-
   events() {
     // clicking the open modal button
     this.openModalButton.click(this.openModal.bind(this));
 
-
     // clicking the x close modal button
     this.closeModalButton.click(this.closeModal.bind(this));
-
 
     // pushes any key
     $(document).keyup(this.keyPressHandler.bind(this));
   }
-
-
 
   keyPressHandler(e) {
     if (e.keyCode == 27) {
@@ -31,21 +25,14 @@ class Modal {
     }
   }
 
-
-
-
   openModal() {
     this.modal.addClass("modal--is-visible");
     return false;
   }
 
-
-
   closeModal() {
     this.modal.removeClass("modal--is-visible");
   }
-
-
 }
 
 export default Modal;
